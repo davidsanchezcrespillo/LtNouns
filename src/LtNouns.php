58f2d0817851bf7db2c082c0ca5d5e14bc3e51a8
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-09-10 21:16:13 +0200

    Test commit

--- src/LtNouns.php
+++ src/LtNouns.php
@@ -6,9 +6,16 @@ namespace LtWords\LtNouns;
  */
 class LtNouns {
   /**
+   * Generate all declensions for a given noun.
+   * The input must be in nominative singular.
+   * The output will be an array containing all the declensions,
+   * in singular and plural forms.
    */
   public function generateDeclensions($noun)
   {
+	  // Possible endings:
+	  // -ias, -as, -is, -ys, -ia, -a, -e, -ius, -us, -uo
+
 	  return "";
   }
 }
