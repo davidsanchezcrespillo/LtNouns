1dda5dbb4a084a0a7f0068f655194c3d20764f3d
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-09-11 09:55:16 +0200

    Declensions for -as

--- src/LtNouns.php
+++ src/LtNouns.php
@@ -5,6 +5,30 @@ namespace LtWords\LtNouns;
  * Class to generate noun declensions in Lithuanian language.
  */
 class LtNouns {
+  private function generateDeclensionsForAs($root)
+  {
+	  return array(
+	    "singular" => array(
+	      "kas" => $root . "as",
+	      "ko" => $root .  "o",
+	      "ką" => $root . "ą",
+	      "kam" => $root . "ui",
+	      "kame" => $root . "e",
+	      "kuo" => $root . "u",
+	      "o" => $root . "e"
+	    ),
+	    "plural" => array(
+	      "kas" => $root . "ai",
+	      "ko" => $root . "ų",
+	      "ką" => $root . "us",
+	      "kam" => $root . "ams",
+	      "kame" => $root . "uose",
+	      "kuo" => $root . "ais",
+	      "o" => $root . "ai"
+	    )
+	  );
+  }
+ 
   /**
    * Generate all declensions for a given noun.
    * The input must be in nominative singular.
@@ -14,8 +38,12 @@ class LtNouns {
   public function generateDeclensions($noun)
   {
 	  // Possible endings:
-	  // -ias, -as, -is, -ys, -ia, -a, -e, -ius, -us, -uo
+	  // -ias, -as, -is, -ys, -ia, -a, -ė, -ius, -us, -uo
 
+      if (preg_match('/(.*)as$/', $noun, $matches)) {
+		  $root = $matches[1];
+		  return $this->generateDeclensionsForAs($root);
+	  }
 	  return "";
   }
 }
--- tests/LtNounsTest.php
+++ tests/LtNounsTest.php
@@ -16,5 +16,28 @@ class LtNounsTest extends PHPUnit_Framework_TestCase {
 	  $this->assertEquals("", $this->_ltNouns->generateDeclensions(""));
   }
   
+  public function testAs() {
+	  $asDeclensions = $this->_ltNouns->generateDeclensions("vyras");
+	  $this->assertInternalType('array', $asDeclensions);
+	  $this->assertInternalType('array', $asDeclensions['singular']);
+	  $this->assertInternalType('array', $asDeclensions['plural']);
+	  
+	  $this->assertEquals('vyras', $asDeclensions['singular']['kas']);
+	  $this->assertEquals('vyro', $asDeclensions['singular']['ko']);
+	  $this->assertEquals('vyrą', $asDeclensions['singular']['ką']);
+	  $this->assertEquals('vyrui', $asDeclensions['singular']['kam']);
+	  $this->assertEquals('vyre', $asDeclensions['singular']['kame']);
+	  $this->assertEquals('vyru', $asDeclensions['singular']['kuo']);
+	  $this->assertEquals('vyre', $asDeclensions['singular']['o']);
+
+	  $this->assertEquals('vyrai', $asDeclensions['plural']['kas']);
+	  $this->assertEquals('vyrų', $asDeclensions['plural']['ko']);
+	  $this->assertEquals('vyrus', $asDeclensions['plural']['ką']);
+	  $this->assertEquals('vyrams', $asDeclensions['plural']['kam']);
+	  $this->assertEquals('vyruose', $asDeclensions['plural']['kame']);
+	  $this->assertEquals('vyrais', $asDeclensions['plural']['kuo']);
+	  $this->assertEquals('vyrai', $asDeclensions['plural']['o']);
+  }
+  
 }
 
