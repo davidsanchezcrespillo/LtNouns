332db2516fe0da5f7db0719b1402ef4cc6af0fd8
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-09-14 23:19:36 +0200

    Added more terminations: -us, -ias, -jas

--- src/LtNouns.php
+++ src/LtNouns.php
@@ -92,6 +92,126 @@ class LtNouns
       );
   }
  
+  private function generateDeclensionsForIas($root)
+  {
+      return array(
+        "singular" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "ias"),
+          "ko" => $this->performPhoneticalSubstitutions($root .  "io"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "ią"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "iui"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "yje"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "iu"),
+          "o" => $this->performPhoneticalSubstitutions($root . "y")
+        ),
+        "plural" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "iai"),
+          "ko" => $this->performPhoneticalSubstitutions($root . "ių"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "ius"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "iams"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "iuose"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "iais"),
+          "o" => $this->performPhoneticalSubstitutions($root . "iai")
+        )
+      );
+  }
+
+  private function generateDeclensionsForJas($root)
+  {
+      return array(
+        "singular" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "jas"),
+          "ko" => $this->performPhoneticalSubstitutions($root .  "jo"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "ją"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "jui"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "juje"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "ju"),
+          "o" => $this->performPhoneticalSubstitutions($root . "jau")
+        ),
+        "plural" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "jai"),
+          "ko" => $this->performPhoneticalSubstitutions($root . "jų"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "jus"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "jams"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "juose"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "jais"),
+          "o" => $this->performPhoneticalSubstitutions($root . "jai")
+        )
+      );
+  }
+
+  private function generateDeclensionsForUs($root)
+  {
+      return array(
+        "singular" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "us"),
+          "ko" => $this->performPhoneticalSubstitutions($root .  "aus"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "ų"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "ui"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "uje"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "umi"),
+          "o" => $this->performPhoneticalSubstitutions($root . "au")
+        ),
+        "plural" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "ūs"),
+          "ko" => $this->performPhoneticalSubstitutions($root . "ų"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "us"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "ums"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "uose"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "umis"),
+          "o" => $this->performPhoneticalSubstitutions($root . "ūs")
+        )
+      );
+  }
+
+  private function generateDeclensionsForIus($root)
+  {
+      return array(
+        "singular" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "ius"),
+          "ko" => $this->performPhoneticalSubstitutions($root .  "iaus"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "ių"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "iui"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "iuje"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "iumi"),
+          "o" => $this->performPhoneticalSubstitutions($root . "iau")
+        ),
+        "plural" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "iai"),
+          "ko" => $this->performPhoneticalSubstitutions($root . "ių"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "ius"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "iams"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "iuose"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "iais"),
+          "o" => $this->performPhoneticalSubstitutions($root . "iai")
+        )
+      );
+  }
+
+  private function generateDeclensionsForJus($root)
+  {
+      return array(
+        "singular" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "jus"),
+          "ko" => $this->performPhoneticalSubstitutions($root .  "jaus"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "jų"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "jui"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "juje"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "jumi"),
+          "o" => $this->performPhoneticalSubstitutions($root . "jau")
+        ),
+        "plural" => array(
+          "kas" => $this->performPhoneticalSubstitutions($root . "jūs"),
+          "ko" => $this->performPhoneticalSubstitutions($root . "jų"),
+          "ką" => $this->performPhoneticalSubstitutions($root . "jus"),
+          "kam" => $this->performPhoneticalSubstitutions($root . "jams"),
+          "kame" => $this->performPhoneticalSubstitutions($root . "juose"),
+          "kuo" => $this->performPhoneticalSubstitutions($root . "jais"),
+          "o" => $this->performPhoneticalSubstitutions($root . "jūs")
+        )
+      );
+  }
+
   /**
    * Generate all declensions for a given noun.
    * The input must be in nominative singular.
@@ -101,7 +221,17 @@ class LtNouns
   public function generateDeclensions($noun)
   {
       // Possible endings:
-      // -ias, -as, -is, -ys, -ia, -a, -ė, -ius, -us, -uo
+      // -jas, -ias, -as, -is, -ys, -ia, -a, -ė, -ius, -us, -uo
+
+      if (preg_match('/(.*)jas$/', $noun, $matches)) {
+          $root = $matches[1];
+          return $this->generateDeclensionsForJas($root);
+      }
+
+      if (preg_match('/(.*)ias$/', $noun, $matches)) {
+          $root = $matches[1];
+          return $this->generateDeclensionsForIas($root);
+      }
 
       if (preg_match('/(.*)as$/', $noun, $matches)) {
           $root = $matches[1];
@@ -117,7 +247,22 @@ class LtNouns
           $root = $matches[1];
           return $this->generateDeclensionsForE($root);
       }
-      
+
+      if (preg_match('/(.*)ius$/', $noun, $matches)) {
+          $root = $matches[1];
+          return $this->generateDeclensionsForIus($root);
+      }
+
+      if (preg_match('/(.*)jus$/', $noun, $matches)) {
+          $root = $matches[1];
+          return $this->generateDeclensionsForJus($root);
+      }
+
+      if (preg_match('/(.*)us$/', $noun, $matches)) {
+          $root = $matches[1];
+          return $this->generateDeclensionsForUs($root);
+      }
+
       return "";
   }
 }
--- tests/LtNounsTest.php
+++ tests/LtNounsTest.php
@@ -1,8 +1,14 @@
 <?php
  
 use LtWords\LtNouns\LtNouns;
- 
-class LtNounsTest extends PHPUnit_Framework_TestCase {
+
+/**
+ * Unit tests for the LtNouns class.
+ * Some references:
+ * https://lt.wikipedia.org/wiki/Kir%C4%8Diuot%C4%97
+ */ 
+class LtNounsTest extends PHPUnit_Framework_TestCase
+{
  
   private $_ltNouns;
   
@@ -37,6 +43,52 @@ class LtNounsTest extends PHPUnit_Framework_TestCase {
 	  $this->assertEquals('vyruose', $asDeclensions['plural']['kame']);
 	  $this->assertEquals('vyrais', $asDeclensions['plural']['kuo']);
 	  $this->assertEquals('vyrai', $asDeclensions['plural']['o']);
+  }
+
+  public function testIas() {
+	  $asDeclensions = $this->_ltNouns->generateDeclensions("kelias");
+	  $this->assertInternalType('array', $asDeclensions);
+	  $this->assertInternalType('array', $asDeclensions['singular']);
+	  $this->assertInternalType('array', $asDeclensions['plural']);
+	  
+	  $this->assertEquals('kelias', $asDeclensions['singular']['kas']);
+	  $this->assertEquals('kelio', $asDeclensions['singular']['ko']);
+	  $this->assertEquals('kelią', $asDeclensions['singular']['ką']);
+	  $this->assertEquals('keliui', $asDeclensions['singular']['kam']);
+	  $this->assertEquals('kelyje', $asDeclensions['singular']['kame']);
+	  $this->assertEquals('keliu', $asDeclensions['singular']['kuo']);
+	  $this->assertEquals('kely', $asDeclensions['singular']['o']);
+
+	  $this->assertEquals('keliai', $asDeclensions['plural']['kas']);
+	  $this->assertEquals('kelių', $asDeclensions['plural']['ko']);
+	  $this->assertEquals('kelius', $asDeclensions['plural']['ką']);
+	  $this->assertEquals('keliams', $asDeclensions['plural']['kam']);
+	  $this->assertEquals('keliuose', $asDeclensions['plural']['kame']);
+	  $this->assertEquals('keliais', $asDeclensions['plural']['kuo']);
+	  $this->assertEquals('keliai', $asDeclensions['plural']['o']);
+  }
+
+  public function testJas() {
+	  $asDeclensions = $this->_ltNouns->generateDeclensions("vėjas");
+	  $this->assertInternalType('array', $asDeclensions);
+	  $this->assertInternalType('array', $asDeclensions['singular']);
+	  $this->assertInternalType('array', $asDeclensions['plural']);
+	  
+	  $this->assertEquals('vėjas', $asDeclensions['singular']['kas']);
+	  $this->assertEquals('vėjo', $asDeclensions['singular']['ko']);
+	  $this->assertEquals('vėją', $asDeclensions['singular']['ką']);
+	  $this->assertEquals('vėjui', $asDeclensions['singular']['kam']);
+	  $this->assertEquals('vėjuje', $asDeclensions['singular']['kame']);
+	  $this->assertEquals('vėju', $asDeclensions['singular']['kuo']);
+	  $this->assertEquals('vėjau', $asDeclensions['singular']['o']);
+
+	  $this->assertEquals('vėjai', $asDeclensions['plural']['kas']);
+	  $this->assertEquals('vėjų', $asDeclensions['plural']['ko']);
+	  $this->assertEquals('vėjus', $asDeclensions['plural']['ką']);
+	  $this->assertEquals('vėjams', $asDeclensions['plural']['kam']);
+	  $this->assertEquals('vėjuose', $asDeclensions['plural']['kame']);
+	  $this->assertEquals('vėjais', $asDeclensions['plural']['kuo']);
+	  $this->assertEquals('vėjai', $asDeclensions['plural']['o']);
   }
 
   public function testA() {
@@ -89,5 +141,76 @@ class LtNounsTest extends PHPUnit_Framework_TestCase {
   
 	  $this->assertEquals("kačių", $moreDeclensions['plural']['ko']);
   }
-}
+  
+  public function testUs() {
+	  $asDeclensions = $this->_ltNouns->generateDeclensions("alus");
 
+	  $this->assertInternalType('array', $asDeclensions);
+	  $this->assertInternalType('array', $asDeclensions['singular']);
+	  $this->assertInternalType('array', $asDeclensions['plural']);
+	  
+	  $this->assertEquals('alus', $asDeclensions['singular']['kas']);
+	  $this->assertEquals('alaus', $asDeclensions['singular']['ko']);
+	  $this->assertEquals('alų', $asDeclensions['singular']['ką']);
+	  $this->assertEquals('alui', $asDeclensions['singular']['kam']);
+	  $this->assertEquals('aluje', $asDeclensions['singular']['kame']);
+	  $this->assertEquals('alumi', $asDeclensions['singular']['kuo']);
+	  $this->assertEquals('alau', $asDeclensions['singular']['o']);
+
+	  $this->assertEquals('alūs', $asDeclensions['plural']['kas']);
+	  $this->assertEquals('alų', $asDeclensions['plural']['ko']);
+	  $this->assertEquals('alus', $asDeclensions['plural']['ką']);
+	  $this->assertEquals('alums', $asDeclensions['plural']['kam']);
+	  $this->assertEquals('aluose', $asDeclensions['plural']['kame']);
+	  $this->assertEquals('alumis', $asDeclensions['plural']['kuo']);
+	  $this->assertEquals('alūs', $asDeclensions['plural']['o']);
+  }
+
+  public function testJus() {
+	  $asDeclensions = $this->_ltNouns->generateDeclensions("pavojus");
+
+	  $this->assertInternalType('array', $asDeclensions);
+	  $this->assertInternalType('array', $asDeclensions['singular']);
+	  $this->assertInternalType('array', $asDeclensions['plural']);
+	  
+	  $this->assertEquals('pavojus', $asDeclensions['singular']['kas']);
+	  $this->assertEquals('pavojaus', $asDeclensions['singular']['ko']);
+	  $this->assertEquals('pavojų', $asDeclensions['singular']['ką']);
+	  $this->assertEquals('pavojui', $asDeclensions['singular']['kam']);
+	  $this->assertEquals('pavojuje', $asDeclensions['singular']['kame']);
+	  $this->assertEquals('pavojumi', $asDeclensions['singular']['kuo']);
+	  $this->assertEquals('pavojau', $asDeclensions['singular']['o']);
+
+	  $this->assertEquals('pavojūs', $asDeclensions['plural']['kas']);
+	  $this->assertEquals('pavojų', $asDeclensions['plural']['ko']);
+	  $this->assertEquals('pavojus', $asDeclensions['plural']['ką']);
+	  $this->assertEquals('pavojams', $asDeclensions['plural']['kam']);
+	  $this->assertEquals('pavojuose', $asDeclensions['plural']['kame']);
+	  $this->assertEquals('pavojais', $asDeclensions['plural']['kuo']);
+	  $this->assertEquals('pavojūs', $asDeclensions['plural']['o']);
+  }
+
+  public function testIus() {
+	  $asDeclensions = $this->_ltNouns->generateDeclensions("amžius");
+
+	  $this->assertInternalType('array', $asDeclensions);
+	  $this->assertInternalType('array', $asDeclensions['singular']);
+	  $this->assertInternalType('array', $asDeclensions['plural']);
+	  
+	  $this->assertEquals('amžius', $asDeclensions['singular']['kas']);
+	  $this->assertEquals('amžiaus', $asDeclensions['singular']['ko']);
+	  $this->assertEquals('amžių', $asDeclensions['singular']['ką']);
+	  $this->assertEquals('amžiui', $asDeclensions['singular']['kam']);
+	  $this->assertEquals('amžiuje', $asDeclensions['singular']['kame']);
+	  $this->assertEquals('amžiumi', $asDeclensions['singular']['kuo']);
+	  $this->assertEquals('amžiau', $asDeclensions['singular']['o']);
+
+	  $this->assertEquals('amžiai', $asDeclensions['plural']['kas']);
+	  $this->assertEquals('amžių', $asDeclensions['plural']['ko']);
+	  $this->assertEquals('amžius', $asDeclensions['plural']['ką']);
+	  $this->assertEquals('amžiams', $asDeclensions['plural']['kam']);
+	  $this->assertEquals('amžiuose', $asDeclensions['plural']['kame']);
+	  $this->assertEquals('amžiais', $asDeclensions['plural']['kuo']);
+	  $this->assertEquals('amžiai', $asDeclensions['plural']['o']);
+  }
+}
